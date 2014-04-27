9d8ab93c53508832612ec2ce24308b5052317c7e
Jeff Brandon <jeffbrandon2010@gmail.com>
2014-04-27 14:58:13 -0400

    Moved note from main documentation to README.md

--- README.md
+++ README.md
@@ -2,3 +2,11 @@ GoBoundedBuffer
 ===============
 
 Bounded Buffer implementation using go. See documentation.
+
+I am still relatively new to go when this is
+being written so I am unaware of what structures
+are best and/or necessary for this problem. I 
+am using goroutines for asynchronus processes.
+This project is based on an assignment 
+I received in an operating systems course at Central
+Michigan University.
--- bb.go
+++ bb.go
@@ -29,14 +29,6 @@ const SIZE int = 200
    After operations on the channel are complete it is
    important to close them to return allocated 
    resources to the system.  
-
-   Note: I am still relatively new to go when this is
-   being written so I am unaware of what structures
-   are best and/or necessary for this problem. I 
-   am using goroutines for asynchronus processes.
-   This project is based on an assignment 
-   I received in an operating systems course at Central
-   Michigan University. 
 */
 
 func main(){
