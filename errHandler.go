e15d7e53e03a42174a8467394796aa04cb5e44d9
Jeff Brandon <jeffbrandon2010@gmail.com>
2014-04-29 16:23:29 -0400

    Accidentally deleted documentation of error handling function. Documentation is re-added here.

--- errHandler.go
+++ errHandler.go
@@ -5,6 +5,11 @@ import (
    "os"
 )
 
+/* Problem: need to handle potential errors
+   Solution: Check if the error exists. If it does print a
+   relevant message to stderr and exit the program.
+*/
+
 func processErr(err error){
    if err != nil {
       fmt.Fprintf(os.Stderr, "Error: %v\n", err)
